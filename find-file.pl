a70b268a445013c0933e83089176dbc26dead3db
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-08-12 16:01:54 +0200

    Not working writing the algorithm

--- find-file.pl
+++ find-file.pl
@@ -3,18 +3,34 @@
 
 #if the argument don"t contain the the file and the project
 my $num_args = $#ARGV+1
-if($num_args <= 2) {
-	print "\n Usage <project>.gpr <file_to_find> [-p platform]";
+if($num_args <= 2 || $num_args>3) {
+    print "\nUsage <project>.gpr <file_to_find> [-p platform]";
 }
-
 #open the gpr file and parse all the folder to search in 
 open(FILE, $ARGV[0]) or die "Cannot open the file".$ARGV[0];
 my $content; 
 local $/;
 #take all the content of the gpr file to parse
 $content=FILE;
-my string_folder;
-string_folder = ($content=~/for\s*Source_Dirs\s*use\((.*)\);/gi);:w
+#if no platform specified (only 1 platform for the compilation)
+if($num_args==2) { 
+#take all the folder used in the gpr file
+    my @string_folder;
+    @string_folder = ($content=~ /for\s+Source_Dirs\s+use\((.*)\);/gi);
+    if($#string_folder != 1) {
+	print "\nthere is more than one platform specified for the compilation";
+    }
+    else {
+	print "\nParsing the sources list ...";
+	#TODO parse $string_folder[0] here 
+    }
+}
+#if $num_args==3
+#parse the platfrom name and receive the corresponding folder list
+else  {
+    my @sring_folder; 
+    @string_folder = ($content=~ /\s*case\s+\(*\w+\)*\s+when\s+$ARGV[2]\s+=>\s+for\+*Source_Dirs\s+use\((.*)\);/gi);
+}
 
 #put all the folder in an array
  
